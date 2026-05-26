4f92e17b7ecfda2e168cd3c7997c0e51541d1bec
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-05-26 13:21:11 +0300

    Update gifts.py

--- gifts.py
+++ gifts.py
@@ -1,42 +1,4 @@
-GIFTS = [
-    {
-        "id": 1,
-        "name": "XJ EYE CREAM",
-        "qty": 6
-    },
-    {
-        "id": 2,
-        "name": "XJ DAY CREAM",
-        "qty": 6
-    },
-    {
-        "id": 3,
-        "name": "XJ NIGHT CREAM",
-        "qty": 6
-    },
-    {
-        "id": 4,
-        "name": "XJ CLEANSING OIL",
-        "qty": 6
-    },
-    {
-        "id": 5,
-        "name": "XJ FACE TONER",
-        "qty": 6
-    },
-    {
-        "id": 6,
-        "name": "XJ 24K GOLD SERUM",
-        "qty": 6
-    },
-    {
-        "id": 7,
-        "name": "XJ FACE WASH",
-        "qty": 6
-    },
-    {
-        "id": 8,
-        "name": "XJ MINI PERFUME",
-        "qty": 16
-    }
-]
+.env
+bot.db
+__pycache__/
+*.pyc
